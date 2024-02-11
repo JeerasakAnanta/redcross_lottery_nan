189fefe9a4ca0b20dbb3cd892048c640e24afdd5
JeerasakAnanta <jeerasakananta@gmail.com>
2024-02-11 22:53:12 +0700

    index

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
 <?php
 
+// hader  
+include("/includes/header.php");
+
+// have bar 
+include("./includes/navigation.php");
+
+?>
+
+<body>
+    <?php
+    // have bar 
+    include("./includes/navigation.php");
+    ?>
+
+    <!-- main  concent   -->
+    <main>
+
+    </main>
+
+    <!--  incoude footer -->
+    <?php
+    include("./includes/footer.php")
+    ?>
+
+</body>
+
+</html>
+
+<?php
+
+?>
