3ccedcd383c3699e647334cf75f536ee632e5885
JeerasakAnanta <jeerasakananta@gmail.com>
2024-02-14 00:59:37 +0700

    show  icon

--- index.php
+++ index.php
@@ -8,10 +8,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
+    <title> ตรวจผลรางวัล ด้วยการถ่ายภาพ OCR </title>
+    <link rel="icon" type="image/x-icon" href="Assets/image/RedCross.png">
+
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <title>ตรวจผลรางวัล ด้วยการถ่ายภาพ OCR </title>
+
     <link rel="stylesheet" href="Assets/style.css">
 </head>
 
