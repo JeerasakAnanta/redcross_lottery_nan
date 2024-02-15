78b350cef5ccc99ada00aa285313c1dbfedeebd2
JeerasakAnanta <jeerasakananta@gmail.com>
2024-02-15 20:43:36 +0700

    update delete

--- admin/delete.php
+++ admin/delete.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // Assuming you have a database connection
 include('../connection/connect.php');
@@ -34,40 +33,3 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
         exit();
     }
 }
-=======
-<?php
-// Assuming you have a database connection
-include('../connection/connect.php');
-
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    header("Access-Control-Allow-Origin: *");
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_URL => "http://203.158.173.23:3000/api/lotterie/$id",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "DELETE",
-        CURLOPT_HTTPHEADER => [
-            "x-rapidapi-host: random-facts2.p.rapidapi.com",
-            "x-rapidapi-key: "
-        ],
-    ]);
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-
-        $data = json_decode($response, true); // Decode the JSON response
-        header("Location: welcome.php"); // Redirect to the display page after updating
-        exit();
-    }
-}
->>>>>>> 3d9a6a1e24b814eb988aa9df1902f705cea074f6
