ae5f2152e1f225cafc792dafa7bb69fefa787e24
JeerasakAnanta <jeerasakananta@gmail.com>
2024-02-15 20:43:57 +0700

    update conn

--- connection/connect.php
+++ connection/connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // server dev  
 // $servername = "localhost";
@@ -22,28 +21,3 @@ if ($conn->connect_error) {
 }
 
 // Perform your database operations here...
-=======
-<?php
-// server dev  
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "lottery_db";
-// server dev  
-
-$servername = "localhost";
-$username = "admin";
-$password = "p@ssw0rd";
-$dbname = "lotteries";
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Perform your database operations here...
->>>>>>> 3d9a6a1e24b814eb988aa9df1902f705cea074f6
