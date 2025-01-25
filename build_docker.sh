35e606c88fa1415ecd33255056818a1711cae744
Game <jeerasakananta@gmail.com>
2025-01-25 14:25:35 +0700

    Enhance build script to include Docker image builds for frontend and backend

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,13 @@
+#!/bin/bash 
+
+# Build the docker images 
 docker compose down 
 
+# build the images frontend 
 docker build -t frontend:0.1.0 ./frontend
 
+# build the images backend 
 docker buildx build -t backend_api:0.1.0 ./backend
 
+# use docker compose to start the containers 
 docker compose -f docker-compose.yml up -d
