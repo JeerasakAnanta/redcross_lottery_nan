7580c01bae42e91e5f8d0d1039086040ce177604
Game <jeerasakananta@gmail.com>
2025-01-28 00:38:19 +0700

    feat(admin): add build step for admin dashboard in Docker script

--- build_docker.sh
+++ build_docker.sh
@@ -9,5 +9,8 @@ docker build -t frontend:0.1.0 ./frontend
 # build the images backend 
 docker buildx build -t backend_api:0.1.0 ./backend
 
+# build the images admin dashboard 
+docker build -t admin_dashboard:0.1.0 ./admin
+
 # use docker compose to start the containers 
 docker compose -f docker-compose.yml up -d 
