5d0efea448ecba1d7ceb20a575950200e9c5c658
Game <jeerasakananta@gmail.com>
2025-01-28 12:07:53 +0700

    fix(build): update docker-compose commands for consistency

--- build_docker.sh
+++ build_docker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 
 # Build the docker images 
-docker compose down 
+docker-compose down 
 
 # build the images frontend 
 docker build -t frontend:0.1.0 ./frontend
 
 # build the images backend 
-docker buildx build -t backend_api:0.1.0 ./backend
+docker build -t backend_api:0.1.0 ./backend
 
 # build the images admin dashboard 
 docker build -t admin_dashboard:0.1.0 ./admin
 
 # use docker compose to start the containers 
-docker compose -f docker-compose.yml up -d 
+docker-compose -f docker-compose.yml up -d 
