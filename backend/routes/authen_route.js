07cc20af2161c386c50e84e6cc15b213ef55c94f
Game <jeerasakananta@gmail.com>
2025-02-03 22:42:13 +0700

    chro : update formantion code

--- backend/routes/authen_route.js
+++ backend/routes/authen_route.js
@@ -1,7 +1,7 @@
-const controller = require('../controllers/authen_controller')
-const router = require('express').Router()
+const controller = require("../controllers/authen_controller");
+const router = require("express").Router();
 
-router.post('/login', controller.login)
-router.post('/register', controller.register)
+router.post("/login", controller.login);
+router.post("/register", controller.register);
 
-module.exports = router
+module.exports = router;
