9c06785290afdbdd80b14f28ac0c50bd0af8be60
Game <jeerasakananta@gmail.com>
2025-02-03 23:16:18 +0700

    fix : models int to string new database

--- backend/models/lotteries_model.js
+++ backend/models/lotteries_model.js
@@ -1,15 +1,26 @@
+/**
+ * Defines the Lottery model
+ * @param {Sequelize} sequelize - Sequelize instance
+ * @param {DataTypes} DataTypes - Sequelize data types
+ * @returns {Model} Lottery model
+ */
+
 module.exports = (sequelize, DataTypes) => {
-    const Lottery = sequelize.define("lottery", {
-        lottery_number: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        reward_number: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-    }, {
-        timestamps: true,
-    })
-    return Lottery
-}
+  const Lottery = sequelize.define(
+    "lottery",
+    {
+      lottery_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      reward_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  return Lottery;
+};
