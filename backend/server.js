416960c1a26d4078c03e91481d981a8e88d64219
Game <jeerasakananta@gmail.com>
2025-02-03 22:41:35 +0700

    update :

--- backend/server.js
+++ backend/server.js
@@ -4,39 +4,49 @@ const express = require("express");
 const cors = require("cors");
 const https = require("https");
 const fs = require("fs");
-const  sequelize  = require("./configs/db.config");
+const sequelize = require("./configs/db.config");
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Middleware setup
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON requests
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 
+// Route imports
 const lotteriesRoute = require("./routes/lotteries_route");
 const authenRoute = require("./routes/authen_route");
 const rewardRoute = require("./routes/reward_route");
 
-app.use("/api_recross_ocr", [lotteriesRoute, authenRoute, rewardRoute]);
+// Route setup
+app.use("/", [lotteriesRoute, authenRoute, rewardRoute]);
 
+// Database connection
 sequelize
   .authenticate()
   .then(() => console.log("Connected to MySQL database."))
   .catch((err) => console.error("Unable to connect to the database: ", err));
 
+// SSL options
 const options = {
-  key: process.env.SSL_KEY_PATH ? fs.readFileSync(process.env.SSL_KEY_PATH) : undefined,
-  cert: process.env.SSL_CERT_PATH ? fs.readFileSync(process.env.SSL_CERT_PATH) : undefined,
+  key: process.env.SSL_KEY_PATH
+    ? fs.readFileSync(process.env.SSL_KEY_PATH)
+    : undefined,
+  cert: process.env.SSL_CERT_PATH
+    ? fs.readFileSync(process.env.SSL_CERT_PATH)
+    : undefined,
 };
 
+// Start server
 const port = process.env.NODE_PORT || 3000;
-const server = process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH
-  ? https.createServer(options, app)
-  : app;
+const server =
+  process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH
+    ? https.createServer(options, app)
+    : app;
 
 server.listen(port, () => {
   console.log(
-    `Server is running on port ${
+    `Server is running on ${
       process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH ? "HTTPS" : "HTTP"
     }://${process.env.NODE_HOST}:${port}`
   );
 });
-
