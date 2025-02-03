e917a6721d26df42623269782b55f0a0229b2cec
Game <jeerasakananta@gmail.com>
2025-02-03 22:47:33 +0700

    braing! : add swgger docs api

--- backend/routes/reward_route.js
+++ backend/routes/reward_route.js
@@ -1,13 +1,38 @@
-const controller = require('../controllers/reward_controller')
-const router = require('express').Router()
-const JWTMiddleware = require('../middlewares/jwt')
-const multer = require('multer');
+const controller = require("../controllers/reward_controller");
+const router = require("express").Router();
+const JWTMiddleware = require("../middlewares/jwt");
+const multer = require("multer");
 
 const upload = multer();
 const jwtMiddleware = JWTMiddleware(process.env.JWT_SECRET_KEY);
 
+/**
+ * @swagger
+ * /api_recross_ocr/reward/upload:
+ *   post:
+ *     summary: Upload image to check reward
+ *     tags: [Reward]
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 // router.post('/upload', jwtMiddleware, upload.single('image'), controller.reward)
-router.post('/upload', upload.single('image'), controller.reward)
-router.post('/check_reward', controller.number_reward)
+router.post("/upload", upload.single("image"), controller.reward);
+router.post("/check_reward", controller.number_reward);
 
-module.exports = router
+module.exports = router;
