db4993ec146a479ae8c5dcc10f9c95db7370b7b5
Game <jeerasakananta@gmail.com>
2025-02-06 02:08:54 +0700

    fix : sequelize  style code

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -1,20 +1,21 @@
-const { Sequelize, DataTypes } = require('sequelize')
-const sequelize = require('../configs/db.config')
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../configs/db");
 
-const db = {}
+const db = {};
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-db.lottery = require('./lotteries_model')(sequelize, DataTypes)
-db.authen = require('./authen_model')(sequelize, DataTypes)
+db.lottery = require("./lotteries_model")(sequelize, DataTypes);
+db.user = require("./user_model")(sequelize, DataTypes);
 
-db.sequelize.sync({ force: false })
-    .then(() => {
-        console.log("Sync is Completed");
-    })
-    .catch((err) => {
-        console.log("Failed is Sync", err);
-    })
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Sync is Completed");
+  })
+  .catch((err) => {
+    console.log("Failed is Sync", err);
+  });
 
-module.exports = db
+module.exports = db;
