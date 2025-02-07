c83536ee692e4b8116f77d2c76392a8b1652b410
Game <jeerasakananta@gmail.com>
2025-02-07 16:16:40 +0700

    fix : remove un use code

--- admin/src/components/Showlottery.tsx
+++ admin/src/components/Showlottery.tsx
@@ -3,8 +3,6 @@ import axios from "axios";
 
 const NODE_ENDPOINT_SHOW = import.meta.env.APP_NODE_ENDPOINT_SHOW;
 const NODE_ENDPOINT_DELETE = import.meta.env.APP_NODE_ENDPOINT_DELETE;
-const NODE_ENDPOINT_INSERT = import.meta.env.APP_NODE_ENDPOINT_INSERT;
-
 
 interface LotteryItem {
   id: number;
